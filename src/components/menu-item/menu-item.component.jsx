65a42f2d3d2428a34e42ebaaad17a90d08ad7f25
Purushottam <purushottam@pprajapati.local>
2020-03-20 18:35:03 +0545

    home page created

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MenuItem = ({title}) => (
+  <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+          <span className='subtitle'>Hats now</span>
+        </div>
+      </div>
+);
